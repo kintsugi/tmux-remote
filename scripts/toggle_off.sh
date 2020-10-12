f7d5fef2e0b63f09a9e6d908aabc5f89ea89052d
kintsu <k@kintsu.io>
2020-10-12 00:15:26 -0700

    toggle script works with status

--- scripts/toggle_off.sh
+++ scripts/toggle_off.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+status_left=$(tmux show-environment -g "TMUX_REMOTE_STATUS_LEFT" 2>&1)
+status_left=${status_left#*=}
+status_left=$(echo "$status_left" | sed 's/"//g')
 
 tmux set -u prefix
 tmux set -u key-table
-tmux set -g status-left ""
+tmux set -g status-left "$status_left"
 tmux refresh-client -S
--- scripts/toggle_on.sh
+++ scripts/toggle_on.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+tmux setenv -g TMUX_REMOTE_STATUS_LEFT "$(tmux show-options -g | grep "^status-left " | sed 's/status-left //')"
 tmux set prefix None
 tmux set key-table off
 tmux set -g status-left "#[fg=colour228,bg=colour52] REMOTE >>>  #[bg=default] "
